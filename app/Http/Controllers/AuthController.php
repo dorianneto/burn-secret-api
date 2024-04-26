6c36ee614a630da7aed9304a915616c8c2826f99
Dorian <dorian@heyorca.com>
2024-04-26 14:17:58 -0230

    feat: refactor endpoints

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,69 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request): JsonResponse
     {
         $data = $request->only(['name', 'email', 'password']);
 
-        $validator = Validator::make($data, [
+
+        $validator = validator()->make($data, [
             'name' => ['required', 'string'],
             'email' => ['required', 'email', 'unique:users'],
             'password' => ['required', 'min:6'],
         ]);
 
         if ($validator->fails()) {
-            return $validator->errors();
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 400);
         }
 
         /** @var User $user */
         $user = User::create($data);
 
-        return [
+        return response()->json([
+            'data' => $user,
             'token' => $user->createToken('auth_token')->plainTextToken,
-        ];
+        ]);
     }
 
-    public function auth(Request $request)
+    public function auth(Request $request): JsonResponse
     {
         $credentials = $request->only(['email', 'password']);
 
-        $validator = Validator::make($credentials, [
+        $validator = validator()->make($credentials, [
             'email' => ['required', 'email'],
             'password' => ['required', 'min:6'],
         ]);
 
         if ($validator->fails()) {
-            return $validator->errors();
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+            ], 400);
         }
 
-        if (Auth::attempt($credentials) === false) {
-            return Response::json(['message' => 'Wrong credentials'], 401);
+        if (auth()->attempt($credentials) === false) {
+            return response()->json([
+                'message' => 'Wrong credentials',
+                'errors' => [],
+            ], 401);
         }
 
         /** @var User $user */
         $user = User::where('email', $credentials['email'])->first();
 
-        $user->tokens()->delete();
-
-        return [
+        return response()->json([
+            'data' => $user,
             'token' => $user->createToken('auth_token')->plainTextToken,
-        ];
+        ]);
     }
 
-    public function logout(Request $request)
+    public function logout(): JsonResponse
     {
-        /** @var User $user */
-        $user = Auth::user();
+        auth()->user()->tokens()->delete();
 
-        $user->tokens()->delete();
-
-        return [];
+        return response()->json([], 204);
     }
 }
